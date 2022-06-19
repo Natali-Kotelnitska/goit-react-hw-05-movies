f54c5ce75cccf00bb03df47a8a5dbf8edb945eec
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-06-19 04:07:05 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import NotFoundView from 'ui/NotFoundView';
 
 import Appbar from './AppBar/AppBar';
 // import Cast from './Cast/Cast';
